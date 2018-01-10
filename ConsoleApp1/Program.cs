a088c56bb39c8d678c0930351033eb039d7d8e6e
Vivian <vivianchu0324@gmail.com>
2018-01-10 18:43:44 +0800

    Use LINQ
    exercise about LINQ

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            List<Student> students = student.getStudent();
+            List<StudentScore> studentScores = student.getScore();
+
+            //Console.WriteLine("C# 課程分數加總 : {0}, 平均: {1}", studentScores.Sum(c => c.CsScore), studentScores.Average(c => c.CsScore).ToString("0.00"));
+            //Console.WriteLine("DB 課程分數加總 : {0}, 平均: {1}", studentScores.Sum(c => c.DbScore), studentScores.Average(c => c.DbScore).ToString("0.00"));
+
+            // Use LINQ
+            var studentScoreQuery = from s in students
+                                    join scores in studentScores on s.Id equals scores.Id
+                                    select new
+                                    {
+                                        Id = s.Id,
+                                        Name = s.Name,
+                                        ScoreSum = scores.CsScore + scores.DbScore,
+                                        ScoreAvg = (scores.CsScore + scores.DbScore) / 2
+                                    };
+            foreach(var studentScore in studentScoreQuery)
+            {
+                Console.WriteLine("學生 {0} 分數加總: {1} 平均:{2}", studentScore.Name, studentScore.ScoreSum, studentScore.ScoreAvg.ToString("0.00"));
+            }
+
+            //int money = 123456789;
+            //double p = 0.11029;
+            //Console.WriteLine("{0}", money.FormForMoney());
+            //Console.WriteLine("{0}", p.FormatPercent());
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Int32Extenson
+    {
+        public static string FormForMoney(this int Value)
+        {
+            return Value.ToString("$###,###,###,##0");
+        }
+    }
+
+    public static class DoubleExtension
+    {
+        public static string FormatPercent(this double Value)
+        {
+            return Value.ToString("#0.00%");
+        }
+    }
+
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //public List<Student> getStudent()
+        //{
+        //    List<Student> student = new List<Student>();
+        //    student.Add(new Student("1", "張三"));
+        //    student.Add(new Student("2", "李四")); 
+
+        //    return student;
+        //}
+
+        public List<Student> getStudent()
+        {
+            return new List<Student>(new[] 
+            {
+                new Student(){ Id ="1", Name = "紀五"},
+                new Student(){ Id ="2", Name = "陳六"},
+                new Student(){ Id ="3", Name = "飛七"},
+                new Student(){ Id ="4", Name = "王八"},
+            });
+        }
+
+        public List<StudentScore> getScore()
+        {
+            List<StudentScore> scoreList = new List<StudentScore>();
+
+            scoreList.Add(new StudentScore("1", 63, 78));
+            scoreList.Add(new StudentScore("2", 81, 86));
+            scoreList.Add(new StudentScore("3", 55, 73));
+
+            return scoreList;
+        }
+
+    }
+
+    class StudentScore
+    {
+        public string Id { get; set; }
+        public double CsScore { get; set; }
+        public double DbScore { get; set; }
+        public Dictionary<string, double> CsScoreList;
+        public Dictionary<string, double> DbScoreList;
+
+        public StudentScore()
+        {
+            CsScoreList = new Dictionary<string, double>();
+            DbScoreList = new Dictionary<string, double>();
+        }
+
+        public StudentScore(string id, int csScore, int dbScore)
+        {
+            Id = id;
+            CsScore = csScore;
+            DbScore = dbScore;
+
+            CsScoreList = new Dictionary<string, double>();
+            DbScoreList = new Dictionary<string, double>();
+            CsScoreList.Add(id, CsScore);
+            DbScoreList.Add(id, CsScore);
+        }
+
+    }
+
+
+}
